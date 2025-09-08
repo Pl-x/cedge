c04bdf40ad23a3e8fe2bb20f6bfb7e537731ae53
The Collective <148755759+Pl-x@users.noreply.github.com>
2025-09-08 23:07:41 +0300

    Update index_1.py
    ml head

--- index_1.py
+++ index_1.py
@@ -2,19 +2,23 @@ import os
 import json
 # import sys
 import pandas as pd
-from scikit.tree import DecisionTreeClassifier as dtc
+from sklearn.tree import DecisionTreeClassifier as dtc
+from sklearn.model_selction import train_test_split as tts
+from sklearn.metrics import accuracy_score as how_accurate
 
 def trainer_model():
   grouped_data = pd.read_csv('scores.csv')
   # grouped_data
-  X = grouped_data.drop('outcome')
+  X = grouped_data.drop(columns=['outcome'])
   y = grouped_data['outcome']
+  X_train, X_test, y_train, y_test = tts(X, y, test_size=0.2)
 
   model = dtc()
-  model.fit(X, y)
+  model.fit(X_train, y_train)
   variables = pd.DataFrame({'Team A': ['Manchester City'], 'Team B': ['Manchester United']})
   prediction = model.predict(variables)
-  return prediction
+  accuracy_level = how_accurate(y_test, prediction)
+  return prediction,accuracy_level
 
 
 def main():
@@ -23,9 +27,10 @@ def main():
     if os.path.exists('outcome.json'):
       with open('outcome.json', 'w') as file:
         # file.read()
-        json.dump(prediction, file')
+        json.dump(prediction, file)
+        json.dump(accuracy_level, file)
     else:
-      RaiseException(f'Error: {PathNotExists} File: {file}')
+      RaiseException(f'Error: {'PathNotExists'} File: {file}')
   except FileNotFoundError as e:
      print(e)
   finally:
