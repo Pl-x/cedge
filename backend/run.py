61b987195ddb2c7c53209f55658db7e6f17795f4
Pl-x <justalloh403@gmail.com>
2026-02-23 22:27:15 +0300

    logging to file5

--- backend/run.py
+++ backend/run.py
@@ -1,6 +1,8 @@
+import os
 from src.prback.config import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.getenv("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=false)
